30772c0a909522df5982a5ecd3e5ba1042ed0667
Jose Almeida <jose@MacBook-Pro.hitronhub.home>
2018-05-18 23:50:29 -0700

    add new products

--- src/components/ProductMain.js
+++ src/components/ProductMain.js
@@ -9,37 +9,37 @@ const ProductMain = ()=>(
         <div className="flex flex-2">
             <article>
                 <div className="image fit">
-                    <img src="./images/imagem_produtos_default.png" />
+                    <img src="https://s3.us-east-2.amazonaws.com/evolumbreapp/b1.jpg" width="200" height="150" />
                 </div>
                 <p>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor lorem ipsum.</p>
             </article>
             <article>
                 <div className="image fit">
-                    <img src="./images/imagem_produtos_default.png" />
+                <img src="https://s3.us-east-2.amazonaws.com/evolumbreapp/b2.jpg" width="200" height="150" />
                 </div>
                 <p>Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna non comodo sodales tempus.</p>
             </article>
             <article>
                 <div className="image fit">
-                    <img src="./images/imagem_produtos_default.png" />
+                <img src="https://s3.us-east-2.amazonaws.com/evolumbreapp/b3.jpg" width="200" height="150" />
                 </div>
                 <p>Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna non comodo sodales tempus.</p>
             </article>
             <article>
                 <div className="image fit">
-                    <img src="./images/imagem_produtos_default.png" />
+                <img src="https://s3.us-east-2.amazonaws.com/evolumbreapp/b4.jpg" width="200" height="150" />
                 </div>
                 <p>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor lorem ipsum.</p>
             </article>
             <article>
                 <div className="image fit">
-                    <img src="./images/imagem_produtos_default.png" />
+                <img src="https://s3.us-east-2.amazonaws.com/evolumbreapp/c1.jpg" width="200" height="150" />
                 </div>
                 <p>Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna non comodo sodales tempus.</p>
             </article>
             <article>
                 <div className="image fit">
-                    <img src="./images/imagem_produtos_default.png" />
+                <img src="https://s3.us-east-2.amazonaws.com/evolumbreapp/c2.jpg" width="200" height="150" />
                 </div>
                 <p>Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna non comodo sodales tempus.</p>
             </article>
